808ab32e7c53f8d34147c644dff822268ec8eecb
NRKRaj14397 <nrkparihar1997@gmail.com>
2025-08-18 20:37:55 +0530

    added middleware and error handler function

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req,res,next)=>{
+
+    // console.log("req.body = ",req.body)
+    const {username, email, password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password,10);
+    const newUser = new User({username,email,password:hashedPassword})
+    try{
+        await newUser.save();
+        res.status(201).json({"message":"User created successfully!"});
+    }catch(error){
+        // res.status(500).json({"error":error});
+        // next(errorHandler(550,'Error from signup route!'));
+        next(error);
+    }
+    
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 // import path from "path";
 // import { fileURLToPath } from "url";
 
@@ -26,9 +27,34 @@ mongoose
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 const app = express();
+app.use(express.json()); //request me json accept karne ke liye iska use karte hain.
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT + "!!");
 });
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+
+
+//Middleware
+/***
+ * Jo tumne likha hai wo Express ka global error handling middleware hai.
+  Express me error handler ka special signature hota hai:
+  (err, req, res, next) => { ... }
+
+  Aise middleware tab trigger hota hai jab tum apne code me next(error) call karte ho ya Express khud koi error throw karta hai.
+ * 
+ */
+app.use((err,req, res,next)=>{
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server error';
+
+  return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  });
+
+});
