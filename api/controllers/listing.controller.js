ca136e2e84efcdc75dfdb65484fb909dc2891797
NRKRaj14397 <nrkparihar1997@gmail.com>
2025-08-25 20:45:36 +0530

    Create search API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -69,3 +69,62 @@ export const getListing = async(req,res,next)=>{
     }
 
 }
+
+export const getListings = async(req,res,next)=>{
+
+
+    try {
+
+        // const listings = await Listing.find();  // pehle resolve karo
+        //     listings.forEach(async (doc) => {
+        // const newType = Math.random() > 0.5 ? "rent" : "sale";
+        // doc.type = newType;
+        // await doc.save();
+        // });
+
+
+        const limit = parseInt(req.query.limit) || 5;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        /**Dono hi case me offer ho yaa naa ho sabhi dikhe */
+        if(offer===undefined || offer==='false'){
+            offer = {$in: [false,true]}
+        }
+
+        let furnished = req.query.furnished
+        if(furnished===undefined || furnished==='false'){
+            furnished = {$in: [false,true]}
+        }
+
+        let parking = req.query.parking
+        if(parking===undefined || parking=='false'){
+            parking = {$in: [false,true]}
+        }
+
+        let type = req.query.type
+        if(type===undefined || type==='all'){
+            type = {$in: ['sale','rent']}
+        }
+        const searchterm = req.query.searchterm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+
+        const listing = await Listing.find({
+            name:{$regex: searchterm, $options:'i'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort({
+            [sort]:order
+        }).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listing);
+
+        
+    } catch (error) {
+        next(error);
+    }
+
+}
