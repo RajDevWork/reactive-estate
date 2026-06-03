0491d371376987e74e281c479aaabe2c62862c78
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-03 17:00:02 +0530

    refactor: clean up code structure and improve path handling

--- api/index.js
+++ api/index.js
@@ -6,19 +6,15 @@ import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
 import cookieParser from "cookie-parser";
 import path from "path";
-// import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";
 
-// const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// ✅ Explicit path
-// dotenv.config({ path: path.join(__dirname, ".env") });
 dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
-
-// console.log("Mongo URI from env:", MONGO_URI); // debug karo
 
 mongoose
   .connect(MONGO_URI)
@@ -26,45 +22,40 @@ mongoose
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 const app = express();
-app.use(express.json()); //request me json accept karne ke liye iska use karte hain.
+
+app.use(express.json());
 app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT + "!!");
-});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
-app.use("/api/listing",listingRouter);
+// Frontend static files
+app.use(
+  express.static(
+    path.join(__dirname, "..", "client", "dist")
+  )
+);
 
-
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
-// app.get("/*",(req,res)=>{
-//   res.sendFile(path.join(__dirname,'client','dist','index.html'));
-// })
-
+// React/Vite routes
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "..", "client", "dist", "index.html")
+  );
 });
-//Middleware
-/***
- * Jo tumne likha hai wo Express ka global error handling middleware hai.
-  Express me error handler ka special signature hota hai:
-  (err, req, res, next) => { ... }
 
-  Aise middleware tab trigger hota hai jab tum apne code me next(error) call karte ho ya Express khud koi error throw karta hai.
- * 
- */
-app.use((err,req, res,next)=>{
-
+// Error Handler
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal server error';
+  const message = err.message || "Internal server error";
 
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
-    message
+    message,
   });
+});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} !!`);
 });
